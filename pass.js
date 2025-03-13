f8b13ef4f20687d774c1eaec110c7f6c7446c22c
yajnesh <ganigayajnesh26@gmail.com>
2025-03-13 10:21:00 +0530

    Updated the JS code

--- pass.js
+++ pass.js
@@ -1,28 +1,24 @@
 const passwordBox = document.getElementById("password");
-    const length = 12;
+const length = 12;
 
-    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    const number = "0123456789";
-    const symbol = "@#$%^&*()_+~|{}[]<>/-=?";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|{}[]<>/-=?";
 
-    const allChars = upperCase + lowerCase + number + symbol;
+const allChars = upperCase + lowerCase + number + symbol;
 
-    function createPassword() {
-        let password = "";
-
-        while (length > password.length) {
-            password += allChars[Math.floor(Math.random() * allChars.length)];
-        }
-        passwordBox.value = password;
+function createPassword() {
+    let password = "";
+    while (password.length < length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
     }
+    passwordBox.value = password;
+}
 
-    function copyPassword() {
-        passwordBox.select();
-        document.execCommand("copy");
-        alert("copied");
-        const data=prompt("Enter sometdhwik");
-        console.log(data)
-    }
-    
-    document.write("hy")
+function copyPassword() {
+    passwordBox.select();
+    navigator.clipboard.writeText(passwordBox.value)
+        .then(() => alert("Password copied to clipboard"))
+        .catch(err => console.error("Failed to copy:", err));
+}
